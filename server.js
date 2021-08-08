4cef4fa13c71d7b8a01dda1143454c3d62cc1163
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-09 02:45:31 +0300

    finishing the backend

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
-
+const JWKSURI = process.env.JWKSURI
 const app = express();
 app.use(cors());
+const client = jwksClient({
+  jwksUri: JWKSURI,
+}) // to send request to Auth0
 
 const PORT = process.env.PORT || 3001;
 
+function getKey(header, callback){
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
 app.get('/test', (request, response) => {
 
   // TODO: 
@@ -18,7 +27,15 @@ app.get('/test', (request, response) => {
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-
+const token = request.headers.authorization.split(' ')[1];``
+console.log(request.headers);
+response.send('got the token');
+jwt.verify(token, getKey, {}, (error, user) => {
+  if(error){
+    response.send('Your token is invalid')
+  }
+  response.json(user)
+})
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
