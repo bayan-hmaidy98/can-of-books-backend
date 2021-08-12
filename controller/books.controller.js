1bcadb1148d2d42cb727794d08ee5256fe580e84
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-08-12 08:52:18 +0300

    almost finished

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -12,5 +12,43 @@ const getbooks = (req, res) => {
         }
     });
 }
+const createNewBook= (request, response) => {
 
-module.exports = getbooks;
+    const email= request.body.email;
+    const title = req.body.title;
+    // console.log(title);
+    const description = request.body.description;
+   
+    const status = request.body.status;
+  
+    const newBook=new userModel({
+       email:email,
+        books:{
+        title,
+        description,
+        status,
+        }
+      });
+      console.log(newBook);
+      newBookObj.save();
+      response.json(newBook);
+    }
+
+    const deleteBook = (request,response) => {
+        try{
+            const id=req.params.book_id;
+            const {email}=request.query;
+            console.log(id);
+            userModel.findOne({ email: email }, (error, data) => {
+                data[0].books.splice(id,1);
+                data.save();
+                    response.send(data); 
+            })  
+        }
+        catch(error){
+            response.send(error.info);
+        }
+        };
+    
+
+module.exports = {getbooks, createNewBook, deleteBook};
